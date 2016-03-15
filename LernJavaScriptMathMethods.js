38859401ae38e8e68b7fd60060b681b3ede8a773
Olimpiada Lysenko <olimpiadalys@gmail.com>
2016-03-15 21:30:13 +0200

    Math.div
    Add method Math.div

---
+++ LernJavaScriptMathMethods.js
@@ -0,0 +1,10 @@
+//Функция деления
+
+Math.divide = function (argument1, argument2)
+{
+	if (argument1 is Number(Object) && argument2 is Number(Object)&& argument2 !==0)
+		
+		return argument1/argument2;
+}
+else
+	return 0;
